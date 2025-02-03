4eab2528a204ecbde8932ab94c138917d55d272e
Carter Plenge <124809586+CarterPlenge@users.noreply.github.com>
2025-02-03 14:37:24 -0600

    reverted index back to pug

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const path = require('path');
 
 // This is how we connect to a pug view
-// router.get('/', (req, res) => {
-//     res.render('homepage.html'); #from index
-// });
+router.get('/', (req, res) => {
+    res.render('index'); //from index
+});
 
 // This is how we connect to  a html page
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/HTML/homepage.html'));
-});
+// router.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/HTML/homepage.html'));
+// });
 
 router.get('/customer', (req, res) => {
     res.render('customerDashboard');
