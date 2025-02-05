7e92eb579545f46c96356e80283a000bf158869e
Carter Plenge <124809586+CarterPlenge@users.noreply.github.com>
2025-02-05 15:21:44 -0600

    basic placeholder webpages

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const path = require('path');
 
 // This is how we connect to a pug view
-router.get('/', (req, res) => {
-    res.render('index'); //from index
-});
+// router.get('/', (req, res) => {
+//     res.render('index'); //from index
+// });
 
 // This is how we connect to  a html page
-// router.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../public/HTML/homepage.html'));
-// });
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/HTML/homepage.html'));
+});
 
 router.get('/customer', (req, res) => {
     res.render('customerDashboard');
@@ -23,5 +23,26 @@ router.get('/employee', (req, res) => {
 router.get('/signup', (req, res) => {
     res.render('signup');
 });
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.get('/menu', (req, res) => {
+    res.render('menu');
+})
+
+router.get('/reservation', (req, res) => {
+    res.render('reservation');
+})
+
+router.get('/catering', (req, res) => {
+    res.render('catering');
+})
+
+router.get('/order', (req, res) => {
+    res.render('order');
+})
+
 
 module.exports = router;
