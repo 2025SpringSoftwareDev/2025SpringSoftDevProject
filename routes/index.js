7e855d08b7e636e628a8e139e31ff9fa1104ae0d
Jake Schellhorn <j.schellhorn.02@gmail.com>
2025-02-05 14:57:54 -0600

    Formatted card class and created grid for rows

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 
 // This is how we connect to  a html page
  router.get('/', (req, res) => {
-   res.sendFile(path.join(__dirname, '../public/HTML/homepage.html'));
+   res.sendFile(path.join(__dirname, '../public/HTML/menu.html'));
  });
 
 router.get('/customer', (req, res) => {
