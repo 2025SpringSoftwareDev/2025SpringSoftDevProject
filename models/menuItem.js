236c91f5ecf5849a12186ae64e8a9e8f8be81566
Jake Schellhorn <j.schellhorn.02@gmail.com>
2025-03-03 13:16:59 -0600

    /menu populates using DB

--- models/menuItem.js
+++ models/menuItem.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const menuItemSchema = mongoose.Schema({
+    category: {type: String, required: true, unique: true },
     name: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
-    calories: { type: Number, required: true }
+    calories: { type: Number, required: true },
+    imageUrl: {type: String, require: true }
 });
 
 module.exports = mongoose.model("Menu", menuItemSchema);
