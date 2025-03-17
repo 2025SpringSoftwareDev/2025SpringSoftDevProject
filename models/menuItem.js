dc0c904ba1d4d4e0c7e9e0bff5556cebdff63215
Jake Schellhorn <j.schellhorn.02@gmail.com>
2025-03-17 14:50:46 -0500

    menu rendered dynamically

--- models/menuItem.js
+++ models/menuItem.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const menuItemSchema = mongoose.Schema({
-    category: {type: String, required: true, unique: true },
+    type: {type: String, required: true, unique: true },
     name: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
